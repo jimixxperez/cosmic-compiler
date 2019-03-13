b27d6b37a36001e80333e9b45f7b323d8895dca9
jimi <jimi@jimis-MacBook-Pro-6.local>
2019-03-13 08:25:06 +0100

    gathered first concept and trying rust out.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,82 @@
+use std::str;
+extern crate regex;
+use regex::Regex;
+// use std::io::File;
 
-fn main() {
-    let mut content = String::new();
-    let val: i64 = 20;
-    defint(&mut content, val);
-    println!("test string {}", content);
+enum TYPES {
+    int_c,
+    char_c,
+    long_c,
+    float_c,
+    double_c,
+    long_doulbe_c
 }
 
-fn defint(content: &mut String, val: i64) -> () {
-    let x = format!("mov {}, %eax", val);
-    content.push_str(&x);
+enum SYNTAX {
+    openCurlBracket,
+    closed_curlBracket,
+    open_round_bracket,
+    close_round_bracket,
+    semicolon,
+    return_statement
+}
+
+
+struct Token<'b> {
+    word: &'b str,
+    value: &'b str
+}
+
+struct Lexer<'a,'b> {
+    keywords: Vec<&'a str>,
+    token_list: Vec<Token<'b>>
+}
+
+impl<'a, 'b> Lexer<'a, 'b> {
+    fn new(keywords: Vec<&'a str>) -> Lexer<'a, 'b> {
+        let tk = vec![];
+        Lexer {
+            keywords: keywords,
+            token_list: tk
+        }
+    }
+
+    fn extract_token(&self, text: &'b str) -> Vec<Token<'b>>{
+        let tk = Token {
+            word: text,
+            value: "1"
+        };
+        vec![tk]
+    }
+
+    fn register_token(&mut self, token: Token<'b>) {
+       self.token_list.push(token);
+    }
+}
+
+fn matchSubElements(el: &str) -> &str {
+   let x = "test".to_owned();
+   let y = &el;
+    println!("{}, {}", y, el);
+    y
+}
+
+fn main() {
+    let y = "int a  = 1;";
+    let keywords = vec!["int", "long", "{", "}", "="];
+    let mut lexer = Lexer::new(keywords);
+    let val = lexer.extract_token(&y);
+    println!("{}", val[0].word);
+    let t = Token{
+        word:"t",
+        value:"value"
+    } ;
+    lexer.register_token(t);
+    let re = Regex::new(r"\s+").unwrap();
+    let mut i = 0;
+    for s in re.split(&y) {
+        matchSubElements(&s);
+        println!("{} {}",i, s) ;
+        i = i + 1;
+    }
 }
